a57aea184a1fe8b56530cf078e1db8db34cb36a7
Klafyvel <klafyvel@gmail.com>
2017-12-19 17:17:34 +0100

    Téléchargement de flake8 et autopep8

--- vim/install.sh
+++ vim/install.sh
@@ -6,6 +6,11 @@ sudo apt install vim
 echo "\033[31m Downloading Vundle \033[0m"
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
+echo "\033[31m Downloading autopep8 \033[0m"
+pip install --upgrade autopep8
+echo "\033[31m Downloading flake8 \033[0m"
+pip install --upgrade flake8
+
 echo "\033[31m Copying .vimrc \033[0m"
 cp .vimrc ~/
 echo "\033[31m Downloading Vim extensions \033[0m"

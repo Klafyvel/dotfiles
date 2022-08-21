9b2382b234c9ca8b772fb6a1ce134ccdd50c5f4e
Hugo Levy-Falk <hugo@klafyvel.me>
2022-08-21 23:40:48 +0200

    nightfox theme for wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require 'wezterm'
 return {
   font = wezterm.font 'JuliaMono',
-  color_scheme = "Dracula",
+  color_scheme = "nightfox",
   hide_tab_bar_if_only_one_tab=true,
   show_update_window = false,
   check_for_updates = false,

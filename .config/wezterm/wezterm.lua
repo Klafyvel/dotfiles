a6b27e4229929b0d711ca6a2e9145ff662855a70
Hugo Levy-Falk <hugo@klafyvel.me>
2022-08-21 23:12:52 +0200

    Switched to wezterm

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,8 @@
+local wezterm = require 'wezterm'
+return {
+  font = wezterm.font 'JuliaMono',
+  color_scheme = "Dracula",
+  hide_tab_bar_if_only_one_tab=true,
+  show_update_window = false,
+  check_for_updates = false,
+}
